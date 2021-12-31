ae66f7f1e43d847b7554f188f06b08c62418338c
YisusSM <xxchuchoxd@outlook.com>
2021-12-31 18:08:45 -0500

    animation close and open chat

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import '../index.css';
 
 export const Chat = () => {
+    const {chat} = useSelector(state => state.ui)
     return (
         <div>
-            <div className="chat-container">
+            <div className={`chat-container ${!chat?"hidden":""}`}>
                 <div className="chat-container__toolbar">
                     <h6 className="chat-container__toolbar-title">CHAT</h6>
                 </div>
